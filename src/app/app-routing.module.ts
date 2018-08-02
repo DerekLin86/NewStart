a74062affa8de0a55de29be33d92005aff0ef26a
Derek Lin <dereklin592@gmail.com>
2018-08-03 00:24:08 +0800

    1. Convert heroes module into chunk module.
    	modified:   src/app/app-routing.module.ts
    	modified:   src/app/app.module.ts
    	new file:   src/app/heroes/heroes-routing.module.ts
    	new file:   src/app/heroes/heroes.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', loadChildren: './heroes/heroes.module#HeroesModule' }
 ];
 
 @NgModule({
