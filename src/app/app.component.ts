8bb0dca523df2cea95c39feb5016a0c85cd75c47
Derek Lin <dereklin592@gmail.com>
2018-08-05 23:36:38 +0800

    1. Angular-s3 - Display the hero quantity on UI.
    	modified:   src/app/app.component.html
    	modified:   src/app/app.component.ts
    	modified:   src/app/hero.service.ts
    	modified:   src/app/heroes/heroes.component.html
    	modified:   src/app/heroes/heroes.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
+import { HeroService } from './hero.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroQuantity$: Observable<number>;
+
+  constructor(private heroService: HeroService) {
+    heroService.getHeroes()
+    .subscribe(heroes => {
+      heroService.updateHerosQutantity(heroes.length);
+    });
+    this.heroQuantity$ = heroService.getHeroesQutantity();
+  }
 }
